8f35cbbb0ca4585ee9d7c006f3ed11802603840f
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 16:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+
+  }
+
+  blocksTravelled() {
+
+  }
+
+  estimatedTime() {
+
+  }
+}
