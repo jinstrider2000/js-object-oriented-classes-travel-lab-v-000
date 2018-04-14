c2c1aa67d60cc970c891ce7dee03cda74f7688a6
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 18:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ class Driver {
 
 class Route {
   constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
 
+  static get eastWest() {
+    return ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
   }
 
   blocksTravelled() {
-
+    const horizontalDistanceCallback = (location) => {
+      return location === this.horizontal;
+    }
+    const verticalDistanceTravelled = Math.abs(beginningLocation.vertical - endingLocation.vertical);
+    const horizontalDistanceTravelled = Math.abs(Route.eastWest.findIndex(horizontalDistanceCallback.bind(this.beginningLocation)) - Route.eastWest.findIndex(horizontalDistanceCallback.bind(this.endingLocation)));
+    return verticalDistanceTravelled + horizontalDistanceTravelled;
   }
 
   estimatedTime() {
